8b2bffbcbcbe742dafcb6dfbf45e4de1fc9c5bf9
Alan6969 <ac91305@eanesisd.net>
2020-11-13 20:50:49 -0600

    Intake motor and servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstJavaOpMode_Alan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstJavaOpMode_Alan.java
@@ -6,9 +6,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//Alan's Code for the Arm
+/*Alan's Code for the Arm
+C:\Users\chuan>cd AppData
+
+C:\Users\chuan\AppData>cd Local
+
+C:\Users\chuan\AppData\Local>cd Android
+
+C:\Users\chuan\AppData\Local\Android>cd sdk
+
+C:\Users\chuan\AppData\Local\Android\Sdk>cd platform-tools
+
+C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb connect 192.168.43.1:5555
+failed to connect to 192.168.43.1:5555
+
+C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb connect 192.168.43.1:5555
+already connected to 192.168.43.1:5555
+
+C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb disconnect
+disconnected everything
+
+C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb connect 192.168.43.1:5555
+connected to 192.168.43.1:5555
+*/
 @TeleOp
 public class MyFirstJavaOpMode_Alan extends LinearOpMode{
+    public DMHardware roboto = new DMHardware();
     private DcMotor motor1;
     private DigitalChannel touchThing;//Maybe
     private Servo servo1;
@@ -17,10 +40,9 @@ public class MyFirstJavaOpMode_Alan extends LinearOpMode{
     @Override
     public void runOpMode() {
         //Init
-        motor1 = hardwareMap.get(DcMotor.class, "motor go brrrr");
-        touchThing = hardwareMap.get(DigitalChannel.class, "touchy");
-        servo1 = hardwareMap.get(Servo.class, "swingyThingTwin");
-        servo2 = hardwareMap.get(Servo.class, "swingyThingTwin2");
+        motor1 = hardwareMap.get(DcMotor.class, "intake_motor");
+        servo1 = hardwareMap.get(Servo.class, "intake_servo_left");
+        servo2 = hardwareMap.get(Servo.class, "intake_servo_right");
         telemetry.addData("Status: ", "Initialized");
         telemetry.update();
 
@@ -30,6 +52,20 @@ public class MyFirstJavaOpMode_Alan extends LinearOpMode{
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
+            roboto.timer.reset();
+
+            motor1.setPower(0.5);
+            servo1.setPosition(0.0);
+            servo2.setPosition(0.0);
+
+            sleep(3000);
+            motor1.setPower(0);
+            servo1.setPosition(1.0);
+            servo2.setPosition(1.0);
+            telemetry.addData("Status", "Terminated");
+            telemetry.update();
+
+            stop();
         }
     }
 }
