4f17c1f38951846cc0f8ea95e586dece7465f783
Alan6969 <ac91305@eanesisd.net>
2021-01-22 20:21:47 -0600

    TeleOp using last years code, only gamepad 1 cause trying to move base right now for this version. Creator: Alan :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstJavaOpMode_Alan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstJavaOpMode_Alan.java
@@ -20,10 +20,10 @@ C:\Users\chuan\AppData\Local\Android\Sdk>cd platform-tools
 C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb connect 192.168.43.1:5555
 failed to connect to 192.168.43.1:5555
 
-C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb connect 192.168.43.1:5555
+C:\Users\chuan\AppData\Local\Android\Sdk\plat form-tools>adb connect 192.168.43.1:5555
 already connected to 192.168.43.1:5555
 
-C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb disconnect
+C:\Users\chuan\AppData\Local\Android\Sdk\platform   -tools>adb disconnect
 disconnected everything
 
 C:\Users\chuan\AppData\Local\Android\Sdk\platform-tools>adb connect 192.168.43.1:5555
@@ -56,12 +56,21 @@ public class MyFirstJavaOpMode_Alan extends LinearOpMode{
 
             motor1.setPower(0.5);
             servo1.setPosition(0.0);
-            servo2.setPosition(0.0);
+            servo2.setPosition(1.0);
+
+            telemetry.addLine("Servo1 =" + servo1.getPosition());
+            telemetry.addLine("Servo2 =" + servo2.getPosition());
+            telemetry.update();
 
             sleep(3000);
             motor1.setPower(0);
             servo1.setPosition(1.0);
-            servo2.setPosition(1.0);
+            servo2.setPosition(0.0);
+            telemetry.addLine("Servo1 =" + servo1.getPosition());
+            telemetry.addLine("Servo2 =" + servo2.getPosition());
+            telemetry.update();
+
+            sleep(5000);
             telemetry.addData("Status", "Terminated");
             telemetry.update();
 
